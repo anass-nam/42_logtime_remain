171a810c98a0dc4e15c45593b895847487a50bab
nammal <anassnammal@gmail.com>
2022-12-21 19:30:09 +0100

    dd

--- content_script.js
+++ content_script.js
@@ -1,20 +1,35 @@
-async function calc(m){
-	const	response = await fetch("https://profile.intra.42.fr/users/<YOUR_INTRA_LOGIN>/locations_stats");
-	const	json = await response.json();
-	let		dates = Object.keys(json).filter(d => new Date(d).getMonth() == m);
-	let		hours = dates.map(d => json[d]);
-	let		total = 0;
-	hours.forEach(e => {
-		total += +e.split(':')[0] * 3600 + +e.split(':')[1] * 60 + +e.split(':')[2];
-	});
-	targ.innerHTML = (`${new Date((total) * 1000).toISOString().substring(11, 16)} hours`);
-}
-
-let		targ = document.querySelector('.user-poste-status');
-targ.style.cursor = 'pointer';
-targ.addEventListener('click', () => {
-	if (targ.innerHTML == "Available")
-		calc(new Date().getMonth());
-	else
-		targ.innerHTML = "Available";
-})
+const fetchData = async () => {
+	try {
+	  const response = await fetch("https://profile.intra.42.fr/users/anammal/locations_stats");
+	  const data = await response.json();
+	  const filtredKeys = Object.keys(data).filter(d => new Date(d).getMonth() === new Date().getMonth());
+	  const filtredHrs = filtredKeys.map(d => data[d]);
+	  let secs = 0;
+	  filtredHrs.forEach(e => {
+		secs += +e.split(':')[0] * 3600 + +e.split(':')[1] * 60 + +e.split(':')[2];
+	  });
+	  const output = timeFormatter(secs);
+	  return output;
+	} catch (error) {
+	  console.error(error);
+	}
+  };
+  
+  const insertHTML = async () => {
+	try {
+	  const htmlResponse = await fetch(chrome.runtime.getURL('./toolbar.html'));
+	  const html = await htmlResponse.text();
+	  document.body.insertAdjacentHTML('beforestart', html);
+	} catch (error) {
+	  console.error(error);
+	}
+  };
+  const updateTime = async () => {
+	const output = await fetchData();
+	// Do something with the output value here, such as updating an element on the page
+	insertHTML();
+  };
+  
+  // Update the time every second
+  setInterval(updateTime, 1000);
+  
---
+++ timeFormatter.js
@@ -0,0 +1,22 @@
+module.expoort = {
+	timeFormatter(sec){
+		let time = {
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+		};
+		while (sec >= 3600) {
+		time.hours++;
+		sec -= 3600;
+		}
+		while (sec >= 60) {
+		time.minutes++;
+		sec -= 60;
+		}
+		time.seconds = sec;
+		Object.keys(time).forEach(keyElm => {
+			time[keyElm] = (time[keyElm] > 9) ? `${time[keyElm]}` : `0${time[keyElm]}`
+		});
+		return Object.values(time).join(':');
+	}
+}
