58fb4666a6a27d75413eab7588156b45b7348bb4
nammal <anassnammal@gmail.com>
2022-12-23 14:44:45 +0100

    updated

--- content_script.js
+++ content_script.js
@@ -1,32 +1,87 @@
-const fetchData = async () => {
+(() => {
+	const css = `
+		main {
+			margin-bottom: 200%;
+		}
+		#time{
+			position: fixed;
+			top: 93.5%;
+			left: 49.9%;
+			font-size: 2em;
+			font-weight: bolder;
+			color: #00babc;
+			z-index: 201;
+		}
+		.floating-menu {
+			
+			z-index: 200;
+			position: fixed;
+			top: 91%;
+			left: 50%;
+			transform: translate(-50,50);
+			border-radius: 50%;
+			border-top: 5px solid #00babc;
+			width: 120px;
+			height: 120px;
+			-webkit-animation: spin 1s linear infinite; /* Safari */
+			animation: spin 1s linear infinite;
+			}
+		/* Safari */
+		@-webkit-keyframes spin {
+		0% { -webkit-transform: rotate(0deg); }
+		100% { -webkit-transform: rotate(360deg); }
+		}
+
+	
+		@keyframes spinr {
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+		}
+
+		
+		`;
+	const styleElement = document.createElement('style');
+	styleElement.innerHTML = css;
+	document.head.appendChild(styleElement);
+	let newMain = document.createElement("main")
+	let nav = document.createElement("nav")
+	let div = document.createElement("div")
+	div.id = 'time'
+	nav.className = "floating-menu"
+	newMain.append(nav)
+	newMain.append(div)
+	document.body.append(newMain)
+})();
+		
+
+const updateTime = async () => {
 	try {
-	  const response = await fetch("https://profile.intra.42.fr/users/anammal/locations_stats");
-	  const data = await response.json();
-	  const filtredKeys = Object.keys(data).filter(d => new Date(d).getMonth() === new Date().getMonth());
-	  const filtredHrs = filtredKeys.map(d => data[d]);
-	  let secs = 0;
-	  filtredHrs.forEach(e => {
-		secs += +e.split(':')[0] * 3600 + +e.split(':')[1] * 60 + +e.split(':')[2];
-	  });
-	  const output = timeFormatter(secs);
-	  return output;
+		const response = await fetch("https://profile.intra.42.fr/users/anammal/locations_stats");
+		const data = await response.json();
+		const currentMonth = new Date().getMonth();
+		const filteredHours = Object.keys(data)
+			.filter(d => new Date(d).getMonth() === currentMonth)
+				.map(d => data[d]);
+		let sec = 0;
+		filteredHours.forEach(e => {
+			const [hours, minutes, seconds] = e.split(":").map(Number);
+			sec += hours * 3600 + minutes * 60 + seconds;
+		});
+		const time = new Time(sec).format(":");
+		document.querySelector('#time').innerHTML = time+'</a>'
 	} catch (error) {
-	  console.error(error);
+		console.error(error);
 	}
-  };
+};
   
-  const insertHTML = async () => {
-	try {
-	  const htmlResponse = await fetch(chrome.runtime.getURL('./toolbar.html'));
-	  const html = await htmlResponse.text();
-	  document.body.insertAdjacentHTML('afterend', html);
-	} catch (error) {
-	  console.error(error);
+  setInterval(updateTime, 1000);
+  
+  class Time {
+	constructor(sec) {
+	  this.hours = String(Math.floor(sec / 3600)).padStart(2, "0");
+	  this.minutes = String(Math.floor((sec % 3600) / 60)).padStart(2, "0");
+	  this.seconds = String(Math.floor(sec % 60)).padStart(2, "0");
 	}
-  };
-  async function updateTime(){
-	const output = await fetchData();
-	insertHTML();
-  };
-  setInterval(updateTime(), 1000);
+	format(d) {return Object.values(this).join(d)}
+  }
   
